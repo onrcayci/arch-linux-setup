19a310233466b7ac59d4e4f480eb05fb0cdd726d
Onur Cayci <onur.cayci@mail.mcgill.ca>
2021-08-02 17:23:20 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Setup yay
 sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
-cd yay && sudo makepkg -sic
+cd yay && makepkg -sic
 
 # Install packages from pacman
 sudo pacman -S firefox flatpak gnome-software-packagekit-plugin noto-fonts noto-fonts-extra powerline ttf-fira-code ttf-roboto zsh
