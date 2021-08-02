c2b261250a15929c3284d3b0139e2d6cc3c840e5
Onur Cayci <onur.cayci@mail.mcgill.ca>
2021-08-02 22:09:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -sic
 
 # Install packages from pacman
-sudo pacman -S firefox flatpak gnome-software-packagekit-plugin noto-fonts noto-fonts-extra powerline ttf-fira-code ttf-roboto zsh
+sudo pacman -S firefox flatpak gnome-software-packagekit-plugin noto-fonts noto-fonts-extra papirus-icon-theme powerline ttf-fira-code ttf-roboto zsh
 
 # Install packages from yay
-yay -S chrome-gnome-shell visual-studio-code-bin
+yay -S chrome-gnome-shell matcha-gtk-theme pamac visual-studio-code-bin
 
 # Change shell to zsh
 chsh -s $(which zsh) ocayci
