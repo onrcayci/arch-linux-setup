5c1b2cfa3d9b5b243a6f37a34e4fc98b5813052e
Onur Cayci <onur.cayci@outlook.com>
2021-08-03 18:40:30 +0000

    Add grub config and mkinitcpio config for plymouth; Update setup.sh to do necessary setups for plymouth and zsh

--- setup.sh
+++ setup.sh
@@ -3,12 +3,22 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -sic
 
+# Cleanup yay
+cd && sudo rm -r yay
+
 # Install packages from pacman
 sudo pacman -S firefox flatpak gnome-software-packagekit-plugin noto-fonts noto-fonts-extra papirus-icon-theme powerline ttf-fira-code ttf-roboto zsh
 
 # Install packages from yay
-yay -S chrome-gnome-shell gnome-shell-extension-dash-to-dock matcha-gtk-theme pamac visual-studio-code-bin
+yay -S chrome-gnome-shell gdm-plymouth gnome-shell-extension-dash-to-dock matcha-gtk-theme pamac plymouth visual-studio-code-bin
+
+# Setup plymouth
+sudo cp arch-linux-setup/mkinitcpio.conf /etc/mkinitcpio.conf
+sudo mkinitcpio -p linux
+sudo cp arch-linux-setup/grub /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Change shell to zsh
 chsh -s $(which zsh) ocayci
+cp arch-linux-setup/.zshrc .zshrc
 echo "Logout and Login again to see the changes take effect."
