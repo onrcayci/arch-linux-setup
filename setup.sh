d4ac9d160ce1c179ce06ef1e9892e5604966158e
Onur Cayci <onur.cayci@outlook.com>
2021-08-27 11:40:02 -0400

    Add flag --needed to the pacman install

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ cd && sudo rm -r yay
 sudo pacman -Rs epiphany gnome-software
 
 # Install packages from pacman
-sudo pacman -S firefox gnome-tweaks noto-fonts noto-fonts-extra papirus-icon-theme powerline ttf-fira-code ttf-roboto
+sudo pacman -S --needed firefox gnome-tweaks noto-fonts noto-fonts-extra papirus-icon-theme powerline ttf-fira-code ttf-roboto
 
 # Install packages from yay
 yay -S chrome-gnome-shell gnome-shell-extension-dash-to-dock visual-studio-code-bin
