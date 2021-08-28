b5e6dd96a5f1e7c13286d63d16dedeacd6831755
Onur Cayci <onur.cayci@outlook.com>
2021-08-27 23:24:35 -0400

    Update nvidia setup script and add setup files

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,11 @@
 # install nvidia drivers
-nvidia nvidia-prime
+sudo pacman -S nvidia nvidia-prime xf86-video-intel
 
 # setup nvidia
 sudo nvidia-xconfig
+
+# copy the rules file into udev
+sudo cp ~/arch-linux-setup/80-nvidia-pm.rules /etc/udev/rules.d/80-nvidia-pm.rules
+
+# copy the configuration file
+sudo cp ~/arch-linux-setup/nvidia-pm.conf /etc/modprobe.d/nvidia-pm.conf
