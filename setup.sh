085484acb95c082f6dd0612983df0f83623b8439
Onur Cayci <onur.cayci@outlook.com>
2021-08-29 10:53:30 -0400

    Update setup.sh script to include new packages; Update setup.sh to setup plymouth boot screen automatically; Update grub amd mkinitcpio.conf files which are needed to setup plymouth correctly

--- setup.sh
+++ setup.sh
@@ -11,11 +11,21 @@ sudo pacman -Rs epiphany gnome-software
 
 # Install packages from pacman
 sudo pacman -S --needed archlinux-wallpaper firefox gnome-tweaks noto-fonts noto-fonts-extra papirus-icon-theme \
-powerline ttf-fira-code ttf-roboto zsh
+powerline pyenv tlp ttf-fira-code ttf-roboto zsh
 
 # Install packages from yay
-yay -S chrome-gnome-shell gnome-shell-extension-dash-to-dock visual-studio-code-bin
+yay -S chrome-gnome-shell gnome-shell-extension-dash-to-dock plymouth gdm-plymouth-prime visual-studio-code-bin
 
 # setup zsh
 chsh -s $(which zsh) ocayci
 cp ~/arch-linux-setup/.zshrc .zshrc
+
+# setup tlp
+sudo systemctl enable tlp
+
+# setup plymouth
+sudo cp ~/arch-linux-setup/mkinitcpio.conf /etc/mkinitcpio.conf
+sudo cp ~/arch-linux-setup/grub /etc/default/grub
+sudo mkinitcpio -P
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermark.png
