c0fd5e27455046997d57ddb358b904f0734d16ce
Onur Cayci <onur.cayci@outlook.com>
2021-08-30 16:14:26 -0400

    Refactor gnome related package installs to gnome script

---
+++ scripts/gnome.sh
@@ -0,0 +1,5 @@
+# install gnome
+sudo pacman -S gnome gnome-tweaks
+
+# install additional goodies
+yay -s chrome-gnome-shell gnome-shell-extension-dash-to-dock
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,15 +6,12 @@ cd yay && makepkg -sic
 # Cleanup yay
 cd && sudo rm -r yay
 
-# Remove unwanted software
-sudo pacman -Rs epiphany gnome-software
-
 # Install packages from pacman
-sudo pacman -S --needed archlinux-wallpaper firefox gnome-tweaks noto-fonts noto-fonts-extra papirus-icon-theme \
+sudo pacman -S --needed archlinux-wallpaper firefox noto-fonts noto-fonts-extra papirus-icon-theme \
 powerline pyenv tlp ttf-fira-code ttf-roboto zsh
 
 # Install packages from yay
-yay -S chrome-gnome-shell gnome-shell-extension-dash-to-dock plymouth gdm-plymouth-prime visual-studio-code-bin
+yay -S plymouth gdm-plymouth-prime visual-studio-code-bin
 
 # setup zsh
 chsh -s $(which zsh) ocayci
