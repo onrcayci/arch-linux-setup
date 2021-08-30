d8723f9b9b0a7d06896496730a900e0b193df04e
Onur Cayci <onur.cayci@outlook.com>
2021-08-29 20:15:04 -0400

    Implement bash script to setup xfce desktop

---
+++ xfce.sh
@@ -0,0 +1,6 @@
+# install base xfce packages
+sudo pacman -S --needed xfce4 xfce4-goodies xfce4-pulseaudio-plugin
+
+# install pulseaudio
+sudo pacman -S --needed bluez blueman pavucontrol pulseaudio \
+pulseaudio-alsa pulseaudio-bluetooth
