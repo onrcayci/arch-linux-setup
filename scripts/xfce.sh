bb38493bd21e86a94867d514987341c640b7b29e
Onur Cayci <onur.cayci@outlook.com>
2021-08-30 16:10:30 -0400

    Update xfce script to add missing dependencies

--- scripts/xfce.sh
+++ scripts/xfce.sh
@@ -1,5 +1,5 @@
 # install base xfce packages
-sudo pacman -S --needed xfce4 xfce4-goodies xfce4-pulseaudio-plugin
+sudo pacman -S --needed gvfs network-manager-applet openssh xfce4 xfce4-goodies xfce4-pulseaudio-plugin
 
 # install pulseaudio
 sudo pacman -S --needed bluez blueman pavucontrol pulseaudio \
