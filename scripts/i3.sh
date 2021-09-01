98c2e119bfd8f98b2e3e9edf42d8c8555839ffa2
Onur Cayci <onur.cayci@outlook.com>
2021-08-31 20:17:23 -0400

    Update i3 script to modify used packages; Add gtk theme configuration

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,9 +1,10 @@
 # install i3 package (i3-gaps)
-sudo pacman -S bluez blueman dmenu feh i3 network-manager-applet \
-openssh pavucontrol pulseaudio pulseaudio-alsa pulseaudio-bluetooth ttf-font-awesome \
-xbacklight xfce4-panel xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin
+sudo pacman -S bluez blueman i3 network-manager-applet nitrogen \
+openssh pavucontrol picom pulseaudio pulseaudio-alsa pulseaudio-bluetooth \
+rofi thunar ttf-font-awesome xfce4-terminal xorg xss-lock
 
 
 # put configuration files
 sudo cp ~/arch-linux-setup/i3/90-touchpad.conf /etc/X11/xorg.conf.d
-cp ~/.arch-linux-setup/i3/config ~/.config/i3
+cp ~/arch-linux-setup/i3/config ~/.config/i3
+cp ~/arch-linux-setup/i3/settings.ini ~/.config/gtk-3.0
