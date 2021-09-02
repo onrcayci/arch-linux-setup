a0f3549302d02110820b4f6161024addf60db809
Onur Cayci <onur.cayci@outlook.com>
2021-09-01 20:47:25 -0400

    Update i3 script to include up-to-date package list of my i3 setup; add config files to the i3 config folder

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,8 +1,9 @@
 # install i3 package (i3-gaps)
-sudo pacman -S bluez blueman i3 network-manager-applet nitrogen \
+sudo pacman -S bluez blueman geoclue i3 network-manager-applet nitrogen \
 openssh pavucontrol picom pulseaudio pulseaudio-alsa pulseaudio-bluetooth \
-rofi thunar ttf-font-awesome xfce4-terminal xorg xss-lock
+redshift rofi thunar ttf-font-awesome xfce4-terminal xorg xss-lock
 
+paru -S polybar
 
 # put configuration files
 sudo cp ~/arch-linux-setup/i3/90-touchpad.conf /etc/X11/xorg.conf.d
@@ -10,3 +11,5 @@ cp ~/arch-linux-setup/i3/config ~/.config/i3
 cp ~/arch-linux-setup/i3/settings.ini ~/.config/gtk-3.0
 cp -r ~/arch-linux-setup/i3/rofi ~/.config
 cp -r ~/arch-linux-setup/i3/i3status ~/.config
+cp -r ~/arch-linux-setup/i3/redshift ~/.config
+cp -r ~/arch-linux-setup/i3/polybar ~/.config
