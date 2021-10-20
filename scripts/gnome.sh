e3d5d67baaa99eb9f24aa64428ccd006113670c4
Onur Cayci <onur.cayci@outlook.com>
2021-10-20 08:24:43 -0400

    Refactor Arch Linux related files into a different repository

--- scripts/gnome.sh
+++
@@ -1,5 +0,0 @@
-# install gnome
-sudo pacman -S gnome gnome-tweaks
-
-# install additional goodies
-yay -s chrome-gnome-shell gnome-shell-extension-dash-to-dock
--- scripts/xfce.sh
+++
@@ -1,6 +0,0 @@
-# install base xfce packages
-sudo pacman -S --needed gvfs network-manager-applet openssh xfce4 xfce4-goodies xfce4-pulseaudio-plugin
-
-# install pulseaudio
-sudo pacman -S --needed bluez blueman pavucontrol pulseaudio \
-pulseaudio-alsa pulseaudio-bluetooth
